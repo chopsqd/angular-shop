06d28d4fbffda107cb1e7437475ef7625a3f2070
Michael Chop <007micle007@gmail.com>
2023-06-10 13:02:13 +0300

    Made the basic layout of the project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from "./common/main-layout/main-layout.component";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {ProductPageComponent} from "./product-page/product-page.component";
+import {CartPageComponent} from "./cart-page/cart-page.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: MainPageComponent},
+      {path: 'product/:id', component: ProductPageComponent},
+      {path: 'cart', component: CartPageComponent},
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
